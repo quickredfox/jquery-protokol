9a0fb53e4d0dd14e43d51033c4b32c283bd2ceea
quickredfox <quickredfox@plac.es>
2010-02-17 09:59:40 -0500

    removed testing until i find a way to write them tests

--- jquery-protokol.js
+++ jquery-protokol.js
@@ -1,5 +1,5 @@
 /*
- * Protokol.js - jQuery/Ajax fake protocol handling
+ * Protokol.js - jQuery custom scheme handling
  * 
  * Copyright (c) 2010 Francois Lafortune (quickredfox.at)
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php 
@@ -18,25 +18,33 @@
         register: function(scheme, type, handler) {
 			if(!$.isReady) $(function(){Protokol.register(scheme, type, handler)});
 			else{
-	            if (!Protokol.proxies[scheme]) Protokol.proxies[scheme] = [];
+				// Tainted handler
 	            var proxy = function(evt, data) {
 	                evt.preventDefault();
 	                handler(evt)
 	            };
+				// Storage for tainted handler
+	            if (!Protokol.proxies[scheme]) Protokol.proxies[scheme] = [];
 	            Protokol.proxies[scheme].push(proxy);
+				// grabs targeted elements with [attr^=scheme]
 	            var elements =
 	            $('[' + (type == 'click' ? 'href': 'action') + '^=' + scheme + ']');
+				// bind listener
 	            elements.bind(type,proxy);				
 			}
+			// make protokol chainable
             return Protokol;
         },
         unregister: function(scheme, type) {
+			// grabs targeted elements with [attr^=scheme]
             var elements =
             $('[' + (type == 'click' ? 'href': 'action') + '^=' + scheme + ']');
+			// unbind stored tainted handlers
             $.each(Protokol.proxies[scheme],
             function(index, handler) {
                 elements.unbind(type, handler)
             });
+			// make protokol chainable
             return Protokol;
         }
     });
